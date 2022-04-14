a611de1c77fa7d01b711a95a4d2a1565da841fa0
a_szczepanik <a.szczepanik@adform.com>
2022-04-14 09:58:18 +0200

    Spark application up and running

--- src/main/scala/MostImportantRows.scala
+++ src/main/scala/MostImportantRows.scala
@@ -1,3 +1,21 @@
-object MostImportantRows {
+import org.apache.spark.sql.SparkSession
 
+object MostImportantRows extends App {
+
+  val spark = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("MostImportantRows")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val input = Seq(
+    (1, "MV1"),
+    (1, "MV2"),
+    (2, "VPV"),
+    (2, "Others")).toDF("id", "value")
+
+  input.show
 }
